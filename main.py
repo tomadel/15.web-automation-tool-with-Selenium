953e0b79f488e86b231902e5dd1173c01f3e0059
Guney <aliguneydogan@hotmail.com>
2025-03-13 16:27:55 +0300

    Added GUI, somebugs are fixed

--- main.py
+++ main.py
@@ -78,7 +78,7 @@ class WebAutomation:
 
 
 
-if __name__ == "__main__"
+if __name__ == "__main__":
     webautomation = WebAutomation()
     webautomation.login("xuser", "xpassword",)
     webautomation.fill_form("Guney Dogan", "guney@gmail.com", "Ruskonlahdenkatu", "Ruskonlahdenkatu")
